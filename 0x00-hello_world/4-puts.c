11ed9ac07150d05be2b7f123749ac15b4b7a6123
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-13 16:10:34 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
-/*This code shows printing output with the 'puts' function*/
-
 #include <stdio.h>
+
+/*
+ 
+   * main - Entry point
+ 
+    *
+ 
+     * Return: Always 0 (Success)
+ 
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

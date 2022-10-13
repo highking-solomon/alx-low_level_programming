dd05b7b04404e56cbc6378695f7b3845cf60517a
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-13 15:54:48 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/*This code shows printing output with the 'puts' function*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}

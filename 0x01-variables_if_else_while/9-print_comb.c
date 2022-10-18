7129be91fa4f0e9802ec9597afd86d8bf310c9ed
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-17 21:31:24 -0700

    all possible number combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "prints the single digit number from 0"
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	int d = '0';
+
+	while (d <= '9')
+	{
+
+		putchar(d);
+
+		if (d <= '8')
+		{
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+		d++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}

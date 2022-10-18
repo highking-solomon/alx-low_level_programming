0489b036449bdc86d861ea4e03c4b474087f4cce
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-17 19:54:58 -0700

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the single digit number from 0
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}

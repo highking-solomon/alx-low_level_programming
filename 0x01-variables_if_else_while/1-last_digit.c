31c8b7c997f298f46512d5eee5cd36df1968480c
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-17 17:11:26 -0700

    more if statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -36,7 +36,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstD);
 	}
-	else
+	else if (lstD == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lstD);
 	}

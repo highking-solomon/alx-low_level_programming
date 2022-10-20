11cf157426511072a77e91b6003e32fd3dd2dc91
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-19 17:53:08 -0700

    isalpha function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _isalpha- check for uppercase characters
+ * _isalpha- check for alphabet
  *
  * @c: character to check
  *
- * Return: 1 if uppercase, else 0
+ * Return: 1 if alphabet, else 0
  *
  * using ASCII characters
- * A=65
- * Z=90
+ * A=65 a=97
+ * Z=90 z=122
  *
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 65 && c <= 122)
 	{
 		return (1);
 	}

0f7e012cb7f71be9f66697ecaf2408b297f0e84b
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-20 16:01:36 -0700

    debug +ve_or -ve

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is +ve or -ve
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+***This project will be working on debugging***

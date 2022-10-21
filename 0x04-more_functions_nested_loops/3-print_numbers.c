0e1933233ed66112b5b60d2f8039a2dd21445375
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-21 16:44:56 -0700

    print number function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers- print 0 - 9
+ *
+ * Return: 0
+ *
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0, c <= 9, c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return 0;
+}

fbb0d41899b05aa04245ca53e52b3a36864ba147
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-21 18:38:10 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Description: Fizzbuzz program
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int f;
+
+	for (f = 1; f<=100; f++)
+	{
+		if (f % 3 == 0 && f % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (f % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (f % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", f);
+		}
+	}
+	return (0);
+}
+

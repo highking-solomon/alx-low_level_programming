097a77a168b2a1d56e6ba37ad5ae4a57f1c57588
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-21 17:01:56 -0700

    print numbers function

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,19 +3,15 @@
 /**
  * print_numbers- print 0 - 9
  *
- * Return: 0
- *
  */
 
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 0, c <= 9, c++)
+	for (i = 0, i <= 9, i++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-
-	return (0);
 }

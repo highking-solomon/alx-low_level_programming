f6726f9dfb869e0ced5da79a1ad416ef8f34aae5
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-10-24 12:31:53 -0700

    string length function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function to count length of string
+ *
+ * @s: string
+ *
+ * Return: string length
+ */
+
+int _strlen(char *s);
+{
+	int i;
+	int strlen = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		strlen++;
+	}
+	return (strlen);
+}

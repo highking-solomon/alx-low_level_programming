5393990468761b04ce3bf4b2ee63793b675e633c
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-11-01 19:21:03 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
-
-
 /**
-
  * _strstr - returns pointer to first char of matching substring
-
  * @haystack: string to find substring in
-
  * @needle: substring to find match of
-
  * Return: pointer to first char of matching substring
-
  */
 
 char *_strstr(char *haystack, char *needle)

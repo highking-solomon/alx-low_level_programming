8e989bb91fda4a651e089174f70a2664d6d31c19
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-11-01 18:48:34 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *_strchr- loops throught the string to find the first occurence of char
+  *
+  *@s: string to check
+  *@c: character being checked for
+  *
+  *Return: s if char found, else return NULL
+  *
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s);
+		else
+			return ('\0');
+	}
+	return (s);
+}

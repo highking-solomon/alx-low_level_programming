2b17f2b477b841f922d9d5c8eba279778afdd2eb
Solomon Olatunbosun <solomon.adetunbosun@gmail.com>
2022-11-02 16:59:19 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_puts_recursion - function that prints a string, then a new line
+  *@s: string to be worked on
+  *
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\0');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+}
